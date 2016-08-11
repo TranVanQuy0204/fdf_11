caee07570a5b0b35e104305d90e9ab0961cd35b3
TranVanQuy0204 <Tran.Van.Quy@framgia.com>
2016-08-11 20:34:40 +0700

    fg-checkout-out-2

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,4 +1,7 @@
 class CheckoutsController < ApplicationController
   def index
+    @order_items = current_order.line_items.joins(:product).
+      select "line_items.*, products.name as product_name,
+      products.image as image"
   end
 end
