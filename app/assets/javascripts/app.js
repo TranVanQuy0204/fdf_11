d3018271259984c3f8c58e3155f4384bf08fa782
quytv <quytv020494@gmail.com>
2016-08-18 02:46:24 +0700

    create-home

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   $('.flash-message').delay(5000).fadeOut(300)
+  $("#sliders").simpleSlider({ speed: 500 });
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+    @categories = Category.all
+    @products = Product.all.take Settings.product_take
+    @order_item = current_order.line_items.new
   end
 end
