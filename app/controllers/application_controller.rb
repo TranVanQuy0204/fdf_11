993c9f14b0261624eba989095dfbb076c6cf698b
quytv <quytv020494@gmail.com>
2016-08-18 04:13:41 +0700

    filter-product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :create_filter
   include OrdersHelper
   include CanCan::ControllerAdditions
+
+  private
+  def create_filter
+    @search = Product.search(params[:q])
+  end
 end
---
+++ app/controllers/filter_products_controller.rb
@@ -0,0 +1,8 @@
+class FilterProductsController < ApplicationController
+  def index
+    @search = Product.search(params[:q])
+    @products = @search.result.order_by_created_at
+      .page_kimanari params[:page]
+    @order_item = current_order.line_items.new
+  end
+end
