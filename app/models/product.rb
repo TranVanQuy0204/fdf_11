a3af4bd006b10a77167438c87637e0181ccfefae
TranVanQuy0204 <Tran.Van.Quy@framgia.com>
2016-08-18 13:51:23 +0700

    admin-crud-product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
   has_many :comments
+
+  class << self
+    def to_csv options = {}
+      CSV.generate options do |csv|
+        csv << column_names
+        all.each do |object|
+          csv << object.attributes.values_at(*column_names)
+        end
+      end
+    end
+  end
 end
