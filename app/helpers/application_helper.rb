99c8dda75daab569209bb1b9189a5b6a0da746f4
TranVanQuy0204 <Tran.Van.Quy@framgia.com>
2016-08-19 08:33:19 +0700

    send-chatwork

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def current_class? test_path
     request.path == test_path ? :active : nil
   end
+
+  def send_message
+    name = ChatWork::Me.get["name"]
+    account_id = ChatWork::Me.get["account_id"]
+    room_id = ChatWork::Me.get["room_id"]
+    message_body = "[To:#{account_id}] #{name} Well done!"
+    ChatWork::Message.create room_id: room_id, body: message_body
+  end
 end
